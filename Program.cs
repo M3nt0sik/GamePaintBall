af69a1d62c1f4c68faff3fa611896c7c2d385b88
Kamil <mentos.96@o2.pl>
2023-05-17 22:44:08 +0200

    Fix fome bugs Fix user saw presed buton on connsole fix no displayed BallsLoaded

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ internal class Program
        PaintballGun Gun = new PaintballGun();
         while (true)
         {
-            Console.WriteLine($"Kulki: {Gun.GetBalls()}, zaladowano: {Gun.GetBallsLoaded}");
-            if (Gun.IsEmpty) Console.WriteLine("Ostrzezenie brak amunicji");
+            Console.WriteLine($"Kulki: {Gun.GetBalls()}, zaladowano: {Gun.GetBallsLoaded()}");
+            if (Gun.IsEmpty()) Console.WriteLine("Ostrzezenie brak amunicji");
             Console.WriteLine("Spacja - strzal, p - przeladowanie," +
                 " + - dodaj amunicje, k - koniec");
-            char key = Console.ReadKey(false).KeyChar;
+            char key = Console.ReadKey(true).KeyChar;
             if (key == ' ') Console.WriteLine($"Proba strzalu: {Gun.Shoot()}");
             else if (key == 'p') Gun.Reload();
             else if (key == '+') Gun.SetBalls(Gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
