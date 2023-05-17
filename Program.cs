8196dc295899c34345e7d300a58178fbd9ed8d52
Kamil <mentos.96@o2.pl>
2023-05-17 22:40:19 +0200

    Write main function

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using GamePaintBall;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       PaintballGun Gun = new PaintballGun();
+        while (true)
+        {
+            Console.WriteLine($"Kulki: {Gun.GetBalls()}, zaladowano: {Gun.GetBallsLoaded}");
+            if (Gun.IsEmpty) Console.WriteLine("Ostrzezenie brak amunicji");
+            Console.WriteLine("Spacja - strzal, p - przeladowanie," +
+                " + - dodaj amunicje, k - koniec");
+            char key = Console.ReadKey(false).KeyChar;
+            if (key == ' ') Console.WriteLine($"Proba strzalu: {Gun.Shoot()}");
+            else if (key == 'p') Gun.Reload();
+            else if (key == '+') Gun.SetBalls(Gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+            else if (key == 'k') return;
+        }
     }
 }
