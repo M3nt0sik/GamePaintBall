8b6bf2038da642a6effe89b13848a1906789c3c7
Kamil <mentos.96@o2.pl>
2023-05-19 21:25:47 +0200

    MOdyfi main function: Replace method GetBalls thought propertis

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ internal class Program
        PaintballGun Gun = new PaintballGun();
         while (true)
         {
-            Console.WriteLine($"Kulki: {Gun.GetBalls()}, zaladowano: {Gun.GetBallsLoaded()}");
+            Console.WriteLine($"Kulki: {Gun.Balls}, zaladowano: {Gun.GetBallsLoaded()}");
             if (Gun.IsEmpty()) Console.WriteLine("Ostrzezenie brak amunicji");
             Console.WriteLine("Spacja - strzal, p - przeladowanie," +
                 " + - dodaj amunicje, k - koniec");
             char key = Console.ReadKey(true).KeyChar;
             if (key == ' ') Console.WriteLine($"Proba strzalu: {Gun.Shoot()}");
             else if (key == 'p') Gun.Reload();
-            else if (key == '+') Gun.SetBalls(Gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+            else if (key == '+') Gun.Balls += PaintballGun.MAGAZINE_SIZE;
             else if (key == 'k') return;
         }
     }
